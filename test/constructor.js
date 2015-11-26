a17728601a7a20536dee043004d230320873a4af
Jason Pincin <jason@pincin.com>
2015-11-26 12:14:13 -0500

    more tests

--- test/constructor.js
+++ test/constructor.js
@@ -3,8 +3,9 @@ var test      = require('tape'),
     timeout   = { timeout: 1000 }
 
 test('exposes a constructor', timeout, function (t) {
-  t.plan(1)
+  t.plan(2)
 
+  t.equal(Eventuate.prototype.constructor, Eventuate)
   var event = new Eventuate
   event.consume(function (data) {
     t.equal(data, 'hi')
