df5af9ef31a030d0dbbaccbe97d8a9e4cf6a1bdb
Jason Pincin <jason@pincin.com>
2015-11-26 10:13:22 -0500

    expose a constructor

---
+++ constructor.js
@@ -0,0 +1,11 @@
+var assign = require('object-assign'),
+    mixin  = require('./mixin')
+
+module.exports = BasicEventuate
+
+function BasicEventuate () {
+  mixin.call(this)
+}
+assign(BasicEventuate.prototype, mixin, {
+  constructor: BasicEventuate
+})
